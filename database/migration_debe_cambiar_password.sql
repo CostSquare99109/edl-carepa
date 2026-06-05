4ce2fbdc573390b692e176ecf0496f7133894d90
CostSquare99109 <jhonfredymontalvocuadrado@gmail.com>
2026-06-05 14:27:50 -0500

    fix: migration SQL corregida (AFTER ultimo_acceso)

--- database/migration_debe_cambiar_password.sql
+++ database/migration_debe_cambiar_password.sql
@@ -1,2 +1,2 @@
 -- Migration: Agregar campo debe_cambiar_password a tabla usuarios
-ALTER TABLE `usuarios` ADD COLUMN `debe_cambiar_password` tinyint(1) NOT NULL DEFAULT 0 AFTER `evaluacion_inicio_febrero`;
+ALTER TABLE `usuarios` ADD COLUMN `debe_cambiar_password` tinyint(1) NOT NULL DEFAULT 0 AFTER `ultimo_acceso`;
